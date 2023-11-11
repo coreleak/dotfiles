8d64a033dd16cb8d6ec106b3499d07e1e16bae0e
coreleak <icebox0227@outlook.com>
2023-11-12 03:52:01 +0800

    :sparkles: Add keymapping support for toggleterm
    :sparkles: now you can move focus window normally while terminal is activated

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -19,3 +19,12 @@ function M.handle_nvimtree_compatibility(terminal)
         api.tree.toggle(false, true)
     end
 end
+
+function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
